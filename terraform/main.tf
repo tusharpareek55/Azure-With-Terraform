7d03e85069129df8d359a3a9ab8f9a6f6f1933c2
Tushar Pareek <tushar.pareek@cloudeq.com>
2023-07-13 12:34:43 +0530

    VM added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,54 @@
-module "RG" {
-  source   = "./RG"
-  rgname   = var.rgname
-  location = var.location
+resource "azurerm_resource_group" "RG" {
+  name     = "example-resources"
+  location = "East US"
 }
-module "SA" {
-  source     = "./SA"
-  sname      = var.sname
-  rgname     = var.rgname
-  location   = var.location
-  depends_on = [module.RG]
+
+resource "azurerm_virtual_network" "VNET" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "SUBNET" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.VNET.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "NIC" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.SUBNET.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "tsr-vm1"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_F2"
+  admin_username      = "tsr"
+  admin_password      = "Tsr@12345678"
+  network_interface_ids = [
+    azurerm_network_interface.NIC.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
