5ba09f74e2f4d139e69552e6bcc0c9fcaf08a8d8
Tushar Pareek <tushar.pareek@cloudeq.com>
2023-07-13 12:51:21 +0530

    VM added

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_subnet" "SUBNET" {
 
 resource "azurerm_public_ip" "my_terraform_public_ip" {
   name                = "public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
   allocation_method   = "Dynamic"
 }
 
